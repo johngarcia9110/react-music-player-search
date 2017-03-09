6a0613d194c931eabc8beae2b1c3af47a0118e94
johngarcia9110 <johngarcia9110@gmail.com>
2017-03-08 22:01:47 -0500

    added app search field and scaffolding

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import { FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
 
 class App extends Component{
     render(){
         return(
-            <div>
-                Music Master From App
+            <div className="app">
+                <div className="title">Spotify Music Search</div>
+                <FormGroup>
+                    <FormControl type="text" 
+                    placeholder="Enter Artist Name"
+                    />
+                    <InputGroup.Addon>
+                        <Glyphicon glyph="search"></Glyphicon>
+                    </InputGroup.Addon>
+                </FormGroup>
+                <div className="artist-profile">
+                    <div>Artist Name</div>
+                    <div>Artist Picture</div>
+                </div>
+                <div className="song-gallery">
+                    Song Gallery
+                </div>
             </div>
+
         )
     }
 }
