dbd736ffd2a5867e4d1a7508e28feaea8b646f11
johngarcia9110 <johngarcia9110@gmail.com>
2017-03-09 22:25:43 -0500

    added GET request to search method

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,35 @@ import React, { Component } from 'react';
 import { FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
 
 class App extends Component{
+    constructor(props){
+        super(props);
+        this.state= {
+            query : ''
+        }
+    }
+
+    search(){
+        const BASE_URL = 'https://api.spotify.com/v1/search?';
+        const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+        console.log('FETCH_URL',FETCH_URL);
+    }
+
     render(){
         return(
             <div className="app">
                 <div className="title">Spotify Music Search</div>
                 <FormGroup>
-                    <FormControl type="text" 
-                    placeholder="Enter Artist Name"
-                    />
-                    <InputGroup.Addon>
-                        <Glyphicon glyph="search"></Glyphicon>
-                    </InputGroup.Addon>
+                    <InputGroup>
+                        <FormControl type="text" 
+                        placeholder="Enter Artist Name"
+                        value={this.state.query}
+                        onChange={event => this.setState({query : event.target.value })}
+                        onKeyPress={event => {if (event.key === 'Enter') { this.search(); }}}
+                        />
+                        <InputGroup.Addon onClick={() => this.search()}>
+                            <Glyphicon glyph="search"></Glyphicon>
+                        </InputGroup.Addon>
+                    </InputGroup>
                 </FormGroup>
                 <div className="artist-profile">
                     <div>Artist Name</div>
