6884539ee96d387291f06c32e73d3dbfc0a50088
johngarcia9110 <johngarcia9110@gmail.com>
2017-03-11 12:11:54 -0500

    added track listings and click to preview functionality

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
 class Gallery extends Component {
+    playAudio(previewUrl){
+        let audio = new Audio(previewUrl);
+        audio.play();
+    }
     render(){
-        console.log('gallery props', this.props);
+        const { tracks } = this.props;
+
         return(
-            <div>Gallery</div>
+            <div className="gallery-wrapper">
+                <h3>Top tracks:</h3>
+                {
+                    tracks.map((track, index) => {
+                        console.log('track', track);
+                        const trackImage = track.album.images[0].url;
+                        return(
+                            <div key={index} 
+                            className="track col-md-4"
+                            onClick={()=> this.playAudio(track.preview_url)}
+                            >
+                                <img src={trackImage} className="track-image" alt="track"/>
+                                <p className="track-title">{track.name}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         )
     }
 }
