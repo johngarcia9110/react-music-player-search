78285e3cd6859181081e851173e48b6bb789256c
johngarcia9110 <johngarcia9110@gmail.com>
2017-03-11 13:02:37 -0500

    added additional style and play pause button

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
 
 class Gallery extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            playingUrl : '',
+            audio : null,
+            playing : false
+        }
+    }
     playAudio(previewUrl){
         let audio = new Audio(previewUrl);
-        audio.play();
+        if(!this.state.playing){
+            audio.play();
+            this.setState({playingUrl : previewUrl, audio, playing : true});
+        }else{
+            if(this.state.playingUrl === previewUrl){
+                this.state.audio.pause();
+                this.setState({playing : false});
+            }else{
+                this.state.audio.pause();
+                audio.play();
+                this.setState({playingUrl : previewUrl, audio, playing : true});
+            }
+        }
     }
     render(){
         const { tracks } = this.props;
@@ -21,7 +41,20 @@ class Gallery extends Component {
                             onClick={()=> this.playAudio(track.preview_url)}
                             >
                                 <img src={trackImage} className="track-image" alt="track"/>
-                                <p className="track-title">{track.name}</p>
+                                <div className="track-play">
+                                    <div className="track-play-inner">
+                                        {
+                                            this.state.playingUrl === track.preview_url 
+                                            ? <span className="track-pause-icon">| |</span>
+                                            : <span className="track-play-icon">&#9654;</span>
+                                        }
+                                    </div>
+                                </div>
+                                <p className="track-title">{
+                                    track.name.length > 10 ? track.name.substring(0,10) + '..' :
+                                    track.name
+                                    
+                                    }</p>
                             </div>
                         )
                     })
